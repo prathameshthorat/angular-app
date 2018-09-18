9a2daded4c15dda5a61d32233766e3d24151a566
prathameshthorat <thoratprathamesh@yahoo.com>
2018-09-18 20:14:53 +0530

    Prathamesh Thorat

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  production: true,
-  FireBase_API_KEY : `${process.env.FireBase_API_KEY}`,
-  FireBase_Auth_Domain : `${process.env.FireBase_Auth_Domain}`
+  production: true
 };
