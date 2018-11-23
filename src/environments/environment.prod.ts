19eac1b47d704228592716b236c12648ccf8a171
prathameshthorat <thoratprathamesh@yahoo.com>
2018-11-23 16:31:05 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   server_api : 'https://angular-app-prathamesh.herokuapp.com/',
+  FireBase_API_KEY : `${process.env.FireBase_API_KEY}`,
+  FireBase_Auth_Domain: `${process.env.FireBase_Auth_Domain}`
 };
