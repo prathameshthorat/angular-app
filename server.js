f045d9f30f810e751f41162b4140865be5ac651c
prathameshthorat <thoratprathamesh@yahoo.com>
2018-11-23 16:05:26 +0530

    ENV Changes

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname + '/dist')));
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Origin', ['http://localhost:4200', 'https://angular-app-prathamesh.herokuapp.com']);
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
