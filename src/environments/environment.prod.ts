e32bcb68826e719009ec99bd62dc6c2113a08dbb
prathameshthorat <thoratprathamesh@yahoo.com>
2018-11-23 11:32:56 +0530

    untracked node_modules

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  server_api : 'https://angular-app-prathamesh.herokuapp.com/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  server_api : 'http://localhost:8080',
+  FireBase_API_KEY : 'AIzaSyCjdiCg7ozKhyLZqSh8zuJe58m6Awcn5Ig',
+  FireBase_Auth_Domain : 'mybankapp-1f03c.firebaseapp.com'
 };
