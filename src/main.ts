4c7b1d4ef49c7a1e4e9720ecd6c2c02f4aed2634
prathameshthorat <thoratprathamesh@yahoo.com>
2018-11-23 12:49:49 +0530

    Deployment changes

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { AuthModule } from './app/auth/auth.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+platformBrowserDynamic().bootstrapModule(AuthModule)
+  .catch(err => console.log(err));
+
